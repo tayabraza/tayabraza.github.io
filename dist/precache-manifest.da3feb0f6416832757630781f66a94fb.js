a2a576fc35204409ddc43432a3826633821c3878
Tayab Raza <tayabraza@gmail.com>
2021-03-10 07:48:45 +0000

    0.0.6

---
+++ dist/precache-manifest.da3feb0f6416832757630781f66a94fb.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28cc38e34f0d8be86dc1",
+    "url": "/tayabraza.github.io.git/css/app.2a3c486e.css"
+  },
+  {
+    "revision": "213bf951dc270f2dc8e5",
+    "url": "/tayabraza.github.io.git/css/chunk-19ed14f3.af9a19b9.css"
+  },
+  {
+    "revision": "deb1ca5a613abe7b77276b8491d9e571",
+    "url": "/tayabraza.github.io.git/img/about.deb1ca5a.jpg"
+  },
+  {
+    "revision": "36b336e471199ef8a0e0d71e6601622d",
+    "url": "/tayabraza.github.io.git/img/contact.36b336e4.jpg"
+  },
+  {
+    "revision": "730317a7472f554f15cd071f4402a36a",
+    "url": "/tayabraza.github.io.git/img/electrician.730317a7.jpg"
+  },
+  {
+    "revision": "44e47fa7ac77fd3add60ca6d80e6ee4c",
+    "url": "/tayabraza.github.io.git/img/heating.44e47fa7.jpg"
+  },
+  {
+    "revision": "b7f5599894d9c53a330620cc9adf5061",
+    "url": "/tayabraza.github.io.git/img/plumber.b7f55998.jpg"
+  },
+  {
+    "revision": "12074c36f61400f9935a256b3f66c22e",
+    "url": "/tayabraza.github.io.git/img/slide-1.12074c36.jpg"
+  },
+  {
+    "revision": "76c9401cfc6d7b5df8e3b5e05dd50c84",
+    "url": "/tayabraza.github.io.git/img/slide-2.76c9401c.jpg"
+  },
+  {
+    "revision": "9aaba1131062170c9102161cb61f15a1",
+    "url": "/tayabraza.github.io.git/img/slide-3.9aaba113.jpg"
+  },
+  {
+    "revision": "aa444e650b0ce56f8cf7d70644b793b1",
+    "url": "/tayabraza.github.io.git/index.html"
+  },
+  {
+    "revision": "28cc38e34f0d8be86dc1",
+    "url": "/tayabraza.github.io.git/js/app.ca392642.js"
+  },
+  {
+    "revision": "9fe41b3d0508b254434d",
+    "url": "/tayabraza.github.io.git/js/chunk-149ec03a.8994aa1e.js"
+  },
+  {
+    "revision": "213bf951dc270f2dc8e5",
+    "url": "/tayabraza.github.io.git/js/chunk-19ed14f3.e8989449.js"
+  },
+  {
+    "revision": "9488944d841dc80323eb",
+    "url": "/tayabraza.github.io.git/js/chunk-86a42c4e.1dc7965c.js"
+  },
+  {
+    "revision": "81390e81ba625251cb4a",
+    "url": "/tayabraza.github.io.git/js/chunk-vendors.a8fcfdc8.js"
+  },
+  {
+    "revision": "4bf7d7c1bafa2affa7782bfc4dbaba0a",
+    "url": "/tayabraza.github.io.git/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/tayabraza.github.io.git/robots.txt"
+  }
+]);
