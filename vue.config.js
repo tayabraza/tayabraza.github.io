132d2970dc4385c7bde8fbeaee2c549314e075c7
Tayab Raza <tayabraza@gmail.com>
2021-03-10 07:46:20 +0000

    0.0.5

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  publicPath: '/',
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/tayabraza.github.io.git/'
+  : '/',
   runtimeCompiler: true,
   lintOnSave: false,
   productionSourceMap: false
